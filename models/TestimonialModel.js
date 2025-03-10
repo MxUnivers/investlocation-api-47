cd1aedec541ef02ea2d1a339df7d22326a2aae5b
angemissiba <as.missiba@b2i-solutions.com>
2025-03-10 16:44:01 +0100

    #V1

--- models/TestimonialModel.js
+++ models/TestimonialModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const testimonialSchema = new Schema({
+    userTestimonial: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
     content: { type: String, required: false },
     description: { type: String },
